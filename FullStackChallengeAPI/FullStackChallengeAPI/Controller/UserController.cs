aa727b9212191f6fa1370acc5f9a2905a5b7f087
LuvuyoSizane <luvuyosizane7@gmail.com>
2025-01-06 21:56:09 +0200

    Started with sample api

---
+++ FullStackChallengeAPI/FullStackChallengeAPI/Controller/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackChallengeAPI.Controller;
+
+public class UserController : Microsoft.AspNetCore.Mvc.Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
